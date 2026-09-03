5e697551b6bac4a8ea9825ff12dba449849864ba
alejandro <aguerreroegido@gmail.com>
2026-09-03 12:08:53 +0100

    Add initial setup for Spring Boot Gateway Service, configured with Maven wrapper, PostgreSQL Docker Compose, and supporting files.

---
+++ src/main/java/com/alejandro/mtogateway/MtoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.mtogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtoGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alejandro/mtogateway/MtoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.mtogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
